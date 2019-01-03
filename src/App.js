5df107e64a8e8dfeaa2972826a8aaf0b21bb7c04
Lauren Piontek <piontekle@gmail.com>
2019-01-03 17:14:10 -0500

    Checkpoint 2 Complete: Read and write the rooms list to the page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import RoomList from './Components/RoomList';
 import * as firebase from 'firebase';
 
 var config = {
@@ -17,20 +17,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <header className="App-header">Bloc Chat App</header>
+        <section id="RoomList">
+          <RoomList firebase={firebase} />
+        </section>
       </div>
     );
   }
---
+++ src/Components/RoomList.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+class RoomList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      rooms: []
+    };
+
+    this.roomsRef = this.props.firebase.database().ref('rooms');
+  }
+
+  componentDidMount() {
+    this.roomsRef.on('child_added', snapshot => {
+      const room = snapshot.val();
+      room.key = snapshot.key;
+      this.setState({ rooms: this.state.rooms.concat( room ) })
+    });
+  }
+
+  render() {
+    return (
+      <section className="room-list">
+        <h1>Bloc Chat Rooms</h1>
+        {
+          this.state.rooms.map ( (roomName, index) =>
+            <p className="room-name" key={index}>{roomName.name}</p>
+          )
+        }
+      </section>
+    );
+  }
+}
+
+export default RoomList;
